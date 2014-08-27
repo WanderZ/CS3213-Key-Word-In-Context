e76365c381d3a82799fa59190a36e0d549af93ca
Andrew Kzk <andrew.kohzk@gmail.com>
2014-08-28 00:15:47 +0800

    adding random comment and testing branching

--- src/ICollate.java
+++ src/ICollate.java
@@ -4,6 +4,9 @@ public interface ICollate {
 	
 	// Under Construction
 
+	// To store data - will be automatically sorted
 	public void storeData(String toStore);
+
+	// To list all the data - will be automatically printed
 	public void listData();
 }
