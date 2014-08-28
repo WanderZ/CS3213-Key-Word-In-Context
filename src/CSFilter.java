d75117dcbe84dec5b99bb70162cc12130c72f038
Andrew Kzk <andrew.kohzk@gmail.com>
2014-08-28 17:39:48 +0800

    concrete Filter, circular shift

---
+++ src/CSFilter.java
@@ -0,0 +1,28 @@
+package src;
+
+public class CSFilter implements IFilter {
+	private String inputString;
+	
+	public CSFilter() {}
+	
+	public CSFilter(String input) {
+		
+	}
+	
+	public void runCircularShift() {
+		
+	}
+
+	@Override
+	public void sendData(Object payload) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveData(Object payload) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
